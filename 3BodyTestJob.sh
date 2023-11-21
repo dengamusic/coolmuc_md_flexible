d9e7389130240a17b4abd11a6dad8cb321621a40
Nick Deng <104977212+dengamusic@users.noreply.github.com>
2023-11-21 14:02:50 +0100

    Update 3BodyTestJob.sh

--- 3BodyTestJob.sh
+++ 3BodyTestJob.sh
@@ -18,5 +18,5 @@ cd $HOME
 for num_threads in 1 2 4 8 16 32 56; do
         export OMP_NUM_THREADS=$num_threads
         AutoPas/build/examples/md-flexible/md-flexible --yaml-file AutoPas/examples/md-flexible/input/3BodyTest.yaml
-        AutoPas/build/examples/md-flexible/md-flexible --yaml-file AutoPas/examples/md-flexible/input/3BodyTest_C01.yaml
+        AutoPas/build/examples/md-flexible/md-flexible --yaml-file AutoPas/examples/md-flexible/input/3BodyTestC01.yaml
 done
