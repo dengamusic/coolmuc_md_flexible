01a1f2cf047901d337ad67fcaa5f996d6c298833
Nick Deng <nanxingnick.deng@tum.de>
2023-11-21 17:07:28 +0100

    save

---
+++ output_parser.py
@@ -0,0 +1,27 @@
+import sys
+import re
+
+if len(sys.argv) != 2:
+    print("Usage: python output_parser.py <filename>")
+    sys.exit(1)
+
+# Get the filename from the command-line argument
+file_name = sys.argv[1]
+
+# Read the content of the file
+try:
+    with open(file_name, 'r') as file:
+        input_text = file.read()
+except FileNotFoundError:
+    print(f"Error: File '{file_name}' not found.")
+    sys.exit(1)
+
+# Define the regular expression pattern
+pattern = re.compile(r'Using (\d+) Threads[\s\S]*?Total wall-clock time\s+: (\d+)', re.MULTILINE)
+
+# Find all matches
+matches = pattern.findall(input_text)
+
+# Display the results
+for i, (threads_used, wall_time_ns) in enumerate(matches, start=1):
+    print(f"Block {i}: Threads Used = {threads_used}, Time = {wall_time_ns} ns")
