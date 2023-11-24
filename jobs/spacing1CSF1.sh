76cca6ee6f8564e6a1418cfe5815d3c648f3995a
Nick Deng <104977212+dengamusic@users.noreply.github.com>
2023-11-24 01:02:59 +0100

    Update spacing1CSF1.sh

--- jobs/spacing1CSF1.sh
+++ jobs/spacing1CSF1.sh
@@ -9,7 +9,7 @@
 #SBATCH --mail-type=end
 #SBATCH --mail-user=nanxingnick.deng@tum.de
 #SBATCH --export=NONE
-#SBATCH --time=03:00:00
+#SBATCH --time=02:00:00
 
 module load slurm_setup
 
@@ -17,9 +17,9 @@ cd $HOME
 
 for num_threads in 1 2 4 8 16 32 56; do
         export OMP_NUM_THREADS=$num_threads
-        AutoPas/build/examples/md-flexible/md-flexible --yaml-file coolmuc_md_flexible/input/all_3b_spacing1_CSF0.5/3BodyTestC01.yaml
-        AutoPas/build/examples/md-flexible/md-flexible --yaml-file coolmuc_md_flexible/input/all_3b_spacing1_CSF0.5/3BodyTestC02Sliced.yaml
-        AutoPas/build/examples/md-flexible/md-flexible --yaml-file coolmuc_md_flexible/input/all_3b_spacing1_CSF0.5/3BodyTestC04.yaml
-        AutoPas/build/examples/md-flexible/md-flexible --yaml-file coolmuc_md_flexible/input/all_3b_spacing1_CSF0.5/3BodyTestC08.yaml
-        AutoPas/build/examples/md-flexible/md-flexible --yaml-file coolmuc_md_flexible/input/all_3b_spacing1_CSF0.5/3BodyTestSliced.yaml
+        AutoPas/build/examples/md-flexible/md-flexible --yaml-file coolmuc_md_flexible/input/all_3b_spacing1_CSF1/3BodyTestC01.yaml
+        AutoPas/build/examples/md-flexible/md-flexible --yaml-file coolmuc_md_flexible/input/all_3b_spacing1_CSF1/3BodyTestC02Sliced.yaml
+        AutoPas/build/examples/md-flexible/md-flexible --yaml-file coolmuc_md_flexible/input/all_3b_spacing1_CSF1/3BodyTestC04.yaml
+        AutoPas/build/examples/md-flexible/md-flexible --yaml-file coolmuc_md_flexible/input/all_3b_spacing1_CSF1/3BodyTestC08.yaml
+        AutoPas/build/examples/md-flexible/md-flexible --yaml-file coolmuc_md_flexible/input/all_3b_spacing1_CSF1/3BodyTestSliced.yaml
 done
