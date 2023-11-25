ad558fdac023d04047191077bd268ac0647c4f69
Nick Deng <104977212+dengamusic@users.noreply.github.com>
2023-11-25 11:56:47 +0100

    Update spacing1CSF025.sh

--- jobs/spacing1CSF025.sh
+++ jobs/spacing1CSF025.sh
@@ -18,8 +18,25 @@ cd $HOME
 for num_threads in 1 2 4 8 16 32 56; do
         export OMP_NUM_THREADS=$num_threads
         AutoPas/build/examples/md-flexible/md-flexible --yaml-file coolmuc_md_flexible/input/all_3b_spacing1_CSF0.25/3BodyTestC01.yaml
+done
+
+for num_threads in 1 2 4 8 16 32 56; do
+        export OMP_NUM_THREADS=$num_threads
         AutoPas/build/examples/md-flexible/md-flexible --yaml-file coolmuc_md_flexible/input/all_3b_spacing1_CSF0.25/3BodyTestC02Sliced.yaml
+done
+
+for num_threads in 1 2 4 8 16 32 56; do
+        export OMP_NUM_THREADS=$num_threads
         AutoPas/build/examples/md-flexible/md-flexible --yaml-file coolmuc_md_flexible/input/all_3b_spacing1_CSF0.25/3BodyTestC04.yaml
+done
+
+
+for num_threads in 1 2 4 8 16 32 56; do
+        export OMP_NUM_THREADS=$num_threads
         AutoPas/build/examples/md-flexible/md-flexible --yaml-file coolmuc_md_flexible/input/all_3b_spacing1_CSF0.25/3BodyTestC08.yaml
+done
+
+for num_threads in 1 2 4 8 16 32 56; do
+        export OMP_NUM_THREADS=$num_threads
         AutoPas/build/examples/md-flexible/md-flexible --yaml-file coolmuc_md_flexible/input/all_3b_spacing1_CSF0.25/3BodyTestSliced.yaml
 done
