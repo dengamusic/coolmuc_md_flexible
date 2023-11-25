4a561a0953a102f2c91bc66a78b2d8155204b046
Nick Deng <104977212+dengamusic@users.noreply.github.com>
2023-11-25 15:39:06 +0100

    Update c08.sh

--- jobs/all_3b_spacing1_CSF1_box20/c08.sh
+++ jobs/all_3b_spacing1_CSF1_box20/c08.sh
@@ -17,5 +17,5 @@ cd $HOME
 
 for num_threads in 1 2 4 8 16 32 56; do
         export OMP_NUM_THREADS=$num_threads
-        AutoPas/build/examples/md-flexible/md-flexible --yaml-file coolmuc_md_flexible/input/all_3b_spacing1_CSF1/3BodyTestC08.yaml
+        AutoPas/build/examples/md-flexible/md-flexible --yaml-file coolmuc_md_flexible/input/all_3b_spacing1_CSF1_box20/3BodyTestC08.yaml
 done
