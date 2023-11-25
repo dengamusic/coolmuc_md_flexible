03e4969898bf5af6594a3c4be9069a7e02191080
Nick Deng <nanxingnick.deng@tum.de>
2023-11-25 12:17:18 +0100

    save

---
+++ jobs/all_3b_spacing1_CSF0.5/sliced.sh
@@ -0,0 +1,42 @@
+#!/bin/bash
+#SBATCH -J 3btest
+#SBATCH --get-user-env
+#SBATCH --clusters=cm2_tiny
+#SBATCH --partition=cm2_tiny
+#SBATCH --nodes=1-1
+#SBATCH --cpus-per-task=56
+# 56 is the maximum reasonable value for CooLMUC-2
+#SBATCH --mail-type=end
+#SBATCH --mail-user=nanxingnick.deng@tum.de
+#SBATCH --export=NONE
+#SBATCH --time=00:45:00
+
+module load slurm_setup
+
+cd $HOME
+
+for num_threads in 1 2 4 8 16 32 56; do
+        export OMP_NUM_THREADS=$num_threads
+        AutoPas/build/examples/md-flexible/md-flexible --yaml-file coolmuc_md_flexible/input/all_3b_spacing1_CSF1/3BodyTestC01.yaml
+done
+
+for num_threads in 1 2 4 8 16 32 56; do
+        export OMP_NUM_THREADS=$num_threads
+        AutoPas/build/examples/md-flexible/md-flexible --yaml-file coolmuc_md_flexible/input/all_3b_spacing1_CSF1/3BodyTestC02Sliced.yaml
+done
+
+for num_threads in 1 2 4 8 16 32 56; do
+        export OMP_NUM_THREADS=$num_threads
+        AutoPas/build/examples/md-flexible/md-flexible --yaml-file coolmuc_md_flexible/input/all_3b_spacing1_CSF1/3BodyTestC04.yaml
+done
+
+
+for num_threads in 1 2 4 8 16 32 56; do
+        export OMP_NUM_THREADS=$num_threads
+        AutoPas/build/examples/md-flexible/md-flexible --yaml-file coolmuc_md_flexible/input/all_3b_spacing1_CSF1/3BodyTestC08.yaml
+done
+
+for num_threads in 1 2 4 8 16 32 56; do
+        export OMP_NUM_THREADS=$num_threads
+        AutoPas/build/examples/md-flexible/md-flexible --yaml-file coolmuc_md_flexible/input/all_3b_spacing1_CSF1/3BodyTestSliced.yaml
+done
