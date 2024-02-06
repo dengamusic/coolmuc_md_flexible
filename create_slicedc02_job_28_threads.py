aacd1ff32e88ef0a29f442a6d849c83ead4e774e
Nick Deng <nanxingnick.deng@tum.de>
2024-02-06 15:55:49 +0100

    save

--- create_slicedc02_job_28_threads.py
+++ create_slicedc02_job_28_threads.py
@@ -24,7 +24,7 @@ def create_yamls_in_directory(directory, spacings, csf, box_size, iterations):
                 if cell_size <= 0.25:
                     create_yaml_file(yaml_file, traversal, spacing, domain, cell_size, 1)
                 else:
-                    create_yaml_file(yaml_file, traversal, spacing, domain, cell_size, 1)
+                    create_yaml_file(yaml_file, traversal, spacing, domain, cell_size, 5)
                 files.append(yaml_file)
 
     return files
@@ -83,7 +83,7 @@ def create_bash_script(directory, yamls):
 
     script_content = f'''\
 #!/bin/bash
-#SBATCH -J wsc1
+#SBATCH -J sc1
 #SBATCH --get-user-env
 #SBATCH --clusters=cm2_tiny
 #SBATCH --partition=cm2_tiny
@@ -93,13 +93,13 @@ def create_bash_script(directory, yamls):
 #SBATCH --mail-type=end
 #SBATCH --mail-user=nanxingnick.deng@tum.de
 #SBATCH --export=NONE
-#SBATCH --time=20:00:00
+#SBATCH --time=10:00:00
 
 module load slurm_setup
 
 cd /dss/dsshome1/05/ge93quw2/
 
-export OMP_NUM_THREADS=1
+export OMP_NUM_THREADS=28
 {yamls_string}
 '''
 
@@ -132,7 +132,7 @@ if __name__ == "__main__":
     # iterations = int(sys.argv[4])
     # duration = sys.argv[5]
     # duration_c01 = sys.argv[6]
-    directory = "slicedc02_1313756_0.8to1.3_0.8to1.3_1thread"
+    directory = "slicedc02_1313756_0.8to1.3_0.8to1.3_28thread"
     spacings = [0.8 + i * 0.05 for i in range(11)]
     csf = [1, 0.5, 0.3333, 0.25, 0.2, 0.16]
     box_size = [13.50001, 13.50001, 756.00001]
