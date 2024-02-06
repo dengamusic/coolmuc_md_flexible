925135b8a93b2ef900f22f52c1bbfa8670db544f
Nick Deng <nanxingnick.deng@tum.de>
2024-02-06 01:02:41 +0100

    save

--- create_sliced_job_28_threads.py
+++ create_sliced_job_28_threads.py
@@ -22,9 +22,9 @@ def create_yamls_in_directory(directory, spacings, csf, box_size, iterations):
                 yaml_file = os.path.join(directory, f"sliced_{spacing}_{cell_size}.yaml")
                 domain = [box_size[0] - spacing, box_size[1] - spacing, box_size[2] - spacing]
                 if cell_size <= 0.25:
-                    create_yaml_file(yaml_file, traversal, spacing, domain, cell_size, 2)
+                    create_yaml_file(yaml_file, traversal, spacing, domain, cell_size, 1)
                 else:
-                    create_yaml_file(yaml_file, traversal, spacing, domain, cell_size, 5)
+                    create_yaml_file(yaml_file, traversal, spacing, domain, cell_size, 1)
                 files.append(yaml_file)
 
     return files
@@ -83,7 +83,7 @@ def create_bash_script(directory, yamls):
 
     script_content = f'''\
 #!/bin/bash
-#SBATCH -J wsl28
+#SBATCH -J wsl1
 #SBATCH --get-user-env
 #SBATCH --clusters=cm2_tiny
 #SBATCH --partition=cm2_tiny
@@ -93,13 +93,13 @@ def create_bash_script(directory, yamls):
 #SBATCH --mail-type=end
 #SBATCH --mail-user=nanxingnick.deng@tum.de
 #SBATCH --export=NONE
-#SBATCH --time=02:00:00
+#SBATCH --time=20:00:00
 
 module load slurm_setup
 
 cd /dss/dsshome1/05/ge93quw2/
 
-export OMP_NUM_THREADS=28
+export OMP_NUM_THREADS=1
 {yamls_string}
 '''
 
@@ -132,10 +132,10 @@ if __name__ == "__main__":
     # iterations = int(sys.argv[4])
     # duration = sys.argv[5]
     # duration_c01 = sys.argv[6]
-    directory = "wsli_1313377_0.8to1.3_1to0.5_28thread"
+    directory = "sli_1313756_0.8to1.3_1thread"
     spacings = [0.8 + i * 0.05 for i in range(11)]
     csf = [1, 0.5, 0.3333, 0.25, 0.2, 0.16]
-    box_size = [13.50001, 13.50001, 378.00001]
+    box_size = [13.50001, 13.50001, 756.00001]
 
     create_directory(directory)
     yamls = create_yamls_in_directory(directory, spacings, csf, box_size, 0)
